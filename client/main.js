343f744177324eac75ea2be73546979286550920
aabi01 <abdullah.amalik07@gmail.com>
2020-07-29 19:32:07 +0200

    initial commit

---
+++ client/main.js
@@ -0,0 +1,29 @@
+var serverURL = 'ws://localhost:8085/';
+var client = new WebSocket(serverURL, 'echo-protocol');
+ 
+client.onerror = function() {
+    console.log('Connection Error');
+};
+ 
+client.onopen = function() {
+    console.log('WebSocket Client Connected');
+ 
+    function sendNumber() {
+        if (client.readyState === client.OPEN) {
+            var number = Math.round(Math.random() * 0xFFFFFF);
+            client.send(number.toString());
+            setTimeout(sendNumber, 1000);
+        }
+    }
+    sendNumber();
+};
+ 
+client.onclose = function() {
+    console.log('echo-protocol Client Closed');
+};
+ 
+client.onmessage = function(e) {
+    if (typeof e.data === 'string') {
+        console.log("Received: '" + e.data + "'");
+    }
+};
